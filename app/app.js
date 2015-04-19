c321a39f15ebfc166839ec6d2241d4b178156b87
Greg Van Buren <g.vanburen@gmail.com>
2015-04-18 17:09:49 -0700

    Adding functionality

--- app/app.js
+++ app/app.js
@@ -1,3 +1,14 @@
-angular.module('calendarDemoApp', []);
-
+angular.module('calendarDemoApp', [])
+.controller('calendarCtrl',['$scope',function($scope){
+	$scope.dateRange = CalendarRange.getMonthlyRange(new Date());
+	console.log($scope.dateRange);
+}])
+.directive('calendar', function(){
+	return {
+		restrict: 'E',
+		transclude: true,
+		templateUrl: 'calendar.html',
+		scope: true,
+	}
+});
 // your controller and directive code go here
